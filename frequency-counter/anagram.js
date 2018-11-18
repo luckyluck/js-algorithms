fdb3cd5295acfcafe886a8c4655d68521934706a
Alexandr Ketov <ketov.alex.v@gmail.com>
2018-11-18 14:58:37 +0600

    Improvement for solution of Anagram challenge using frequency counter.

--- frequency-counter/anagram.js
+++ frequency-counter/anagram.js
@@ -2,23 +2,18 @@ function validAnagram(s1, s2) {
     if (s1.length !== s2.length) return false;
     if (s1 === s2) return true;
 
-    const f1 = {};
-    const f2 = {};
+    const lookup = {};
 
     for (let l of s1) {
-        f1[l] = ++f1[l] || 1;
-    }
-    for (let l of s2) {
-        f2[l] = ++f2[l] || 1;
+        lookup[l] = ++lookup[l] || 1;
     }
 
-    for (const key of Object.keys(f1)) {
-        if (!f2.hasOwnProperty(key)) {
+    for (let l of s2) {
+        if (!lookup[l]) {
             return false;
         }
-        if (f1[key] !== f2[key]) {
-            return false;
-        }
+
+        lookup[l] -= 1;
     }
 
     return true;
